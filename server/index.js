67e17571afd720c6d7132be33cc95df68a63dab4
Jared Ellison <jaredellison@gmail.com>
2018-12-20 15:16:58 -0500

    Implement express server and serve static files

---
+++ server/index.js
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////
+//
+//    Express Server
+
+////////////////////////////////////////
+//  Configuration Constants
+
+const PORT = process.env.PORT || 3010;
+const PUBLIC_DIR = '../public/';
+
+/////////////////////////////////////////
+//  Import dependencies
+
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+// Apply middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+////////////////////
+//  Run Server
+
+app.listen(PORT, (err, data) => {
+  if (err) return console.log('Error starting server:', err);
+  console.log(`Successfully started server on http://localhost:${PORT}`);
+});
