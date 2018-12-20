9cb82db4b2f17310f041bd00b9015dbdabe3f42f
Jared Ellison <jaredellison@gmail.com>
2018-12-20 17:23:01 -0500

    Install test dependencies and write test pseudocode for server and database

---
+++ tests/database.js
@@ -0,0 +1,5 @@
+// Essential tests:
+
+// db.ListingImages model should be capable of saving to the database
+// db.ListingImages model should be capable of reading from the database
+// db.ListingImages.findOne({id: id}) where should resolve to an object with map and floorPlan properties when id < 100
---
+++ tests/server.js
@@ -0,0 +1,7 @@
+// Essential tests:
+
+// Should respond to a request to / with html
+// Should respond to a request to /api/0-100 with json describing a single object
+// API request object should have map property with string value
+// API request object should have floorPlan property with string value
+// API request object should have a photos property with array value at least one item long
