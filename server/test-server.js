311279947d5b12cdedcf168b17928f98741ad357
Jared Ellison <jaredellison@gmail.com>
2019-01-02 00:52:00 -0500

    Rename files for consistency

--- server/test-server.js
+++
@@ -1,7 +0,0 @@
-// Essential tests:
-
-// Should respond to a request to / with html
-// Should respond to a request to /api/0-100 with json describing a single object
-// API request object should have map property with string value
-// API request object should have floorPlan property with string value
-// API request object should have a photos property with array value at least one item long
